0e5a4bf1c025d1da9c47093f10aac0710c4231e1
2nonnymouse <47648494+2nonnymouse@users.noreply.github.com>
2019-02-16 15:57:08 -0600

    vuerouter
    vuerouter installed

--- src/main.js
+++ src/main.js
@@ -2,13 +2,34 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
 import App from "./App";
+import vueRouter from "vue-router";
+
+import Users from "./components/Users";
+
 //added=vueresorce dependancy
 import vueResource from "vue-resource";
 Vue.config.productionTip = false;
+
 Vue.use(vueResource);
+Vue.use(vueRouter);
+
+const router = new VueRouter({
+  mode: "history",
+  base: __dirname,
+  routes: [{ path: "/", component: Users }]
+});
+
 /* eslint-disable no-new */
 new Vue({
-  el: "#app",
-  components: { App },
-  template: "<App/>"
-});
+  //  el: "#app",
+  //  components: { App },
+  //  template: "<App/>"
+  router,
+  template: `
+    <div id="app">
+    <ul>
+      <li><router.link to="/">Users</router-link></li>
+    </ul>
+  <router-view></router-view>
+  </div>`
+}).$mount("#app");
